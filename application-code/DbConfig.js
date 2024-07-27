af2457c5ef3c307918e7a3e95fbbe306ecf30828
Kehinde Iyanda <141333790+KehindeoIyanda@users.noreply.github.com>
2024-07-27 03:13:34 -0400

    Delete application-code/DbConfig.js

--- application-code/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cluster-clwoq0iei8qd.us-east-2.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'Ganiyuolaniyan$123',
-    DB_DATABASE : 'webappdb'
-});
